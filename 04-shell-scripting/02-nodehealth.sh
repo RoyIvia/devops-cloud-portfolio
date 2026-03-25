bb6d638e51f84ab4998f03f222f5095c3ad528ca
Roy Ivia <107504526+RoyIvia@users.noreply.github.com>
2026-03-25 11:16:04 +0300

    Update 02-nodehealth.sh

--- 04-shell-scripting/02-nodehealth.sh
+++ 04-shell-scripting/02-nodehealth.sh
@@ -34,26 +34,3 @@ echo "Show Amazon processes"
 
 ps -ef | grep "amazon"
 
-echo "Amazon processes but case insensitive "
-
-ps -ef | grep -i "amazon"
-
-#Now it will match:
-#amazon
-#Amazon
-#AMAZON
-#AmaZon
-
-
-#This command:
- ps -ef | grep amazon
-
-#will also show the grep process itself, like: root     1234  ... grep amazon
-
-#Option 1: exclude grep
-
-ps -ef | grep -i "amazon" | grep -v grep
-
-#Option 2 (BEST practice):
-
-pgrep -af amazon
